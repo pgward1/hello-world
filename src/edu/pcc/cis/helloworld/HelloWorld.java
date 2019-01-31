1e4018dbecc39a24c2a746b41ca3bfa8ca7afd01
Patty Ward <patricia.ward@pcc.edu>
2019-01-31 15:18:06 -0800

    added main method that prints out Hello World-Here is week4 part2.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Crated by Patricia Ward on 31-JAN-2019
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World-Here is week4 part2.");
+    }
 }
