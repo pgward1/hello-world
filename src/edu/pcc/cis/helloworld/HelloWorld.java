8e35aaaada877c28f502910fb75ec2463e5f8837
Patty Ward <patricia.ward@pcc.edu>
2019-01-31 15:23:53 -0800

    added statement to print the current date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,5 @@
 package edu.pcc.cis.helloworld;
-
+import java.util.Date;
 /**
  * Crated by Patricia Ward on 31-JAN-2019
  */
@@ -7,5 +7,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World-Here is week4 part2.");
+        System.out.println(new Date());
     }
 }
